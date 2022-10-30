39fca7e9869da1102fd5f74b003a6f6175ee9d6d
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-10-30 17:47:01 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-green-600 h-screen">
+      <p className="text-white text-3xl">Hello, React + Typescript + Tailwind CSS</p>
+    </div>
+  )
+}
+
+export default App
